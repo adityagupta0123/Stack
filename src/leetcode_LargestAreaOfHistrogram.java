f39f90504a79d4422d515ddaabb79a5be3ae1702
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-07-29 21:58:21 +0530

    Update and rename LargestAreaOfHistrogram.java to leetcode_LargestAreaOfHistrogram.java

--- src/leetcode_LargestAreaOfHistrogram.java
+++ src/leetcode_LargestAreaOfHistrogram.java
@@ -22,7 +22,7 @@ public class LargestAreaOfHistrogram {
         st.push(0 );
         lb[0] = -1;
         for(int i= 1; i < arr.length ; i++){
-            while(st.size() > 0 && arr[i] < arr[st.peek()])
+            while(st.size() > 0 && arr[i] <= arr[st.peek()])
                 st.pop();
             if(st.size() == 0)
                 lb[i] = -1;
